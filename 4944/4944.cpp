de373f4b58ad22616019082b2e0d871cb1ac7273
DESKTOP-CO549O6\slous <simon.lousky@campus.technion.ac.il>
2019-05-20 15:41:07 +0300

    4944 working

--- 4944/4944.cpp
+++ 4944/4944.cpp
@@ -3,6 +3,8 @@
 #include<stdio.h>
 #include<set>
 #include<algorithm>
+#include<tuple>
+#include<queue>
 
 #define EPS 1e-9
 
@@ -13,116 +15,77 @@ typedef vector<int> vi;
 typedef vector<pair<int,int>> vpi;
 typedef pair<int, int> pii;
 
-struct greater1{
-  bool operator()(const pii& a,const pii& b) const{
-      if(a.first == b.first)
-      {
-          return a.second > b.second;
-      }
-      return a.first < b.first;
-  }
-};
+bool cmp(const pii& a,const pii& b){
+    if(a.first == b.first)
+    {
+        return a.second < b.second;
+    }
+    return a.first > b.first;
+}
 
 int main()
 {
     int cases;
     scanf("%d", &cases);
-    
     for(int i=0; i<cases; ++i){
-        ll p, n, part, sum;
+        int n, p, sum;
         sum = 0;
         vi a;
         vpi remain;
-        vpi tafran;
         bool impossible=false;
         scanf("%d %d", &p, &n);
-        part = p / n;
-        sum = part * n;
         for(int j=0; j<n; ++j)
         {   
             int tmp;
             scanf("%d", &tmp);
-            a.push_back(part);
-            remain.push_back(pii(tmp-part, j));
-            if(tmp-part < 0)
-            {
-                tafran.push_back(pii(abs(tmp-part), j));
-            }
+            sum += tmp;
+            a.push_back(0);
+            remain.push_back(pii(tmp, j));
+            // cout << "n: " << n << " p: " << p << endl;
         }
-        make_heap(remain.begin(), remain.end(), greater1());
-        make_heap(tafran.begin(), tafran.end(), greater1());
-        
-        while(sum < p)
+        if(sum < p)
         {
-            cout << "sum: " << sum << ", p:" << p << endl;
-            pii r = remain.front();
-            pop_heap(remain.begin(), remain.end(), greater1());
-            remain.pop_back();
-            int r_idx = r.second;
-            int r_value = r.first;
-
-            r_value -= 1;
-            sum++;
-
-            remain.push_back(pii(r_value, r_idx));
-            push_heap(remain.begin(), remain.end(), greater1());
-            a[r_idx] += 1;
-
-            if(r_value < 0)
-            {
-                impossible=true;
-            }
+            impossible = true;
         }
-        for (auto u: a)
-            cout << u << ' ';
-        while(tafran.size() && !impossible)
+        sort(remain.begin(), remain.end(), cmp);
+        while(p > 0 && !impossible)
         {
-
-            pii t = tafran.front();
-            pop_heap(tafran.begin(), tafran.end(), greater1());
-            tafran.pop_back();
-            int t_idx = t.second;
-            int t_value = t.first;
-
-            pii r = remain.front();
-            pop_heap(remain.begin(), remain.end(), greater1());
-            remain.pop_back();
-            int r_idx = r.second;
-            int r_value = r.first;
-
-            r_value -= 1;
-            t_value -= 1;
-            a[t_idx] -= 1;
-            a[r_idx] += 1;
-
-
-            if(r_value >= 0)
+            int needed_beat = p / remain.size(); int rmd = p % remain.size();
+            int next_beat = remain.back().first; int idx = remain.back().second;
+            if(needed_beat < next_beat)
             {
-                remain.push_back(pii(r_value, r_idx));
-                push_heap(remain.begin(), remain.end(), greater1());
+                next_beat = needed_beat;
             }
-            if(t_value != 0)
+            if(needed_beat == 0)
             {
-                tafran.push_back(pii(t_value, t_idx));
-                push_heap(tafran.begin(), tafran.end(), greater1());
+                next_beat = 1;
             }
-            // for (auto u: a)
-            //     cout << u << ' ';
-            // cout << endl;
-            // for (auto u: tafran)
-            //     cout << u.first << ' ';
-            // cout << endl;
-            // for (auto u: remain)
-            //     cout << u.second << ' ';
-            // cout << endl;
+
+            // cout << "beat: " << next_beat << endl;
+            for(auto& payer : remain)
+            {
+                p -= next_beat;
+                pii new_value = pii(payer.first - next_beat, payer.second);
+                payer = new_value;
+                a[payer.second] += next_beat;
+
+                if(p == 0)
+                {
+                    break;
+                }
+            }
+            // remove all empty handed
+            for(auto j = remain.rbegin(); remain.back().first == 0; ++j)
+            {
+                remain.pop_back();
+            }
         }
-        int diff = *max_element(a.begin(), a.end()) - *min_element(a.begin(), a.end());
-        // printf("diff: %d\n", *min_element(a.begin(), a.end()));
-        if(impossible || diff > part || diff < 0)
+
+        if(impossible)
         {
             cout << "IMPOSSIBLE" << endl;
-            cout << "diff: " << diff << endl;
-            // continue;
+            // cout << "diff: " << diff << endl;
+            continue;
         }
         for(int k=0; k<n-1; ++k)
         {
@@ -130,6 +93,6 @@ int main()
         }
         cout << a[n-1] << endl;
     }
-    cout << endl;
+    // cout << endl;
     return 0;
 }
