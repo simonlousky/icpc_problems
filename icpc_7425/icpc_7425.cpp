cd3766cf70456a1f6fa46c7e43bce1e29f7268a4
DESKTOP-CO549O6\slous <simon.lousky@campus.technion.ac.il>
2019-06-23 23:59:34 +0300

    7425 working

--- icpc_7425/icpc_7425.cpp
+++ icpc_7425/icpc_7425.cpp
@@ -17,58 +17,7 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef long long ll;
 
-/* ================================================= */
-/* ************************************************** */
-
-struct PT {
-  double x, y;
-  PT() {}
-  PT(double x, double y) : x(x), y(y) {}
-  PT(const PT &p) : x(p.x), y(p.y)    {}
-  PT operator + (const PT &p)  const { return PT(x+p.x, y+p.y); }
-  PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
-  PT operator * (double c)     const { return PT(x*c,   y*c  ); }
-  PT operator / (double c)     const { return PT(x/c,   y/c  ); }
-  bool operator !=(const PT &p)  const { return x != p.x || y != p.y ; }
-};
-
-double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
-double dist2(PT p, PT q)   { return dot(p-q,p-q); }
-double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
-ostream &operator<<(ostream &os, const PT &p) {
-  os << "(" << p.x << "," << p.y << ")";
-}
-
-// determine if lines from a to b and c to d are parallel or collinear
-bool LinesParallel(PT a, PT b, PT c, PT d) {
-  return fabs(cross(b-a, c-d)) < EPS;
-}
-
-bool LinesCollinear(PT a, PT b, PT c, PT d) {
-  return LinesParallel(a, b, c, d)
-      && fabs(cross(a-b, a-c)) < EPS
-      && fabs(cross(c-d, c-a)) < EPS;
-}
-
-bool SegmentsIntersect(PT a, PT b, PT c, PT d) {
-  if (LinesCollinear(a, b, c, d)) {
-    if (dist2(a, c) < EPS || dist2(a, d) < EPS ||
-      dist2(b, c) < EPS || dist2(b, d) < EPS) return true;
-    if (dot(c-a, c-b) > 0 && dot(d-a, d-b) > 0 && dot(c-b, d-b) > 0)
-      return false;
-    return true;
-  }
-  if (cross(d-a, b-a) * cross(c-a, b-a) > 0) return false;
-  if (cross(a-c, d-c) * cross(b-c, d-c) > 0) return false;
-  return true;
-}
-
-/****************************************
- * *************************************?
-
-
 /* =========== CG functions ============ */
-
 
 struct point { double x, y;   // only used if more precision is needed
     point() { x = y = 0.0; }                      // default constructor
@@ -80,7 +29,6 @@ struct point { double x, y;   // only used if more precision is needed
     // use EPS (1e-9) when testing equality of two floating points
     bool operator == (point other) const {
         return (fabs(x - other.x) < EPS && (fabs(y - other.y) < EPS)); } };
-
 
 struct line { double a, b, c; };          // a way to represent a line
 
@@ -153,31 +101,27 @@ bool areIntersect(line l1, line l2, point &p) {
     else                  p.y = -(l2.a * p.x + l2.c);
     return true; }
 
-
 bool segmentIntersect(segment& a, segment& b, point &p) {
     line l1, l2;
     pointsToLine(a.a, a.b, l1);
     pointsToLine(b.a, b.b, l2);
     
-    // check first if have common points
+    /* check first if have common points otherwise segments on the same line
+    won't be recognized as intersecting */
     if(a.a == b.a || a.a == b.b || a.b == b.a || a.b == b.b){
         return true;
     }
 
     if(areIntersect(l1, l2, p)){
-        // cout << "intersection candidate: " << p.x << "," << p.y << endl;
         double left = a.get_left().x < b.get_left().x ? b.get_left().x : a.get_left().x;
         double top = a.get_up().y < b.get_up().y ? a.get_up().y : b.get_up().y;
         double down = a.get_down().y < b.get_down().y ? b.get_down().y : a.get_down().y;
         double right = a.get_right().x < b.get_right().x ? a.get_right().x : b.get_right().x;
-        // cout << "get_right: " << b.get_right().x << endl;
-        // cout << "left: " << left << ", right: " << right << ", top: " << top << ", down: " << down << endl;
         if( (p.x > right && fabs(p.x - right) > EPS)
         ||  (p.x < left && fabs(p.x - left) > EPS)
         ||  (p.y < down && fabs(p.y - down) > EPS)
         ||  (p.y > top && fabs(p.y - top) > EPS) )
         {
-            // cout << "candidate is intersection" << endl;
             return false;
         }
     
@@ -186,78 +130,17 @@ bool segmentIntersect(segment& a, segment& b, point &p) {
     return false;
 }
 
-bool segmentIntersect(segment& a, double x_line, point& p){
-    segment line_segment;
-    line_segment.a.x = x_line;
-    line_segment.a.y = INT16_MAX;
-    line_segment.b.x = x_line;
-    line_segment.b.y = INT16_MIN;
-    return segmentIntersect(a, line_segment, p);
-}
-
-double segment_to_x(const string& s, const segment& a){
-    double p;
-    if(s == "start"){
-        p = a.a.x < a.b.x ? a.a.x : a.b.x;
-    }else if(s == "end"){
-        p = a.a.x < a.b.x ? a.b.x : a.a.x;
-    }else{
-        p = -1;
-    }
-    return p;
-}
-
-/* ========= End of CG functions ========= */
-
-/* Floyd-Warshal */
-void floyd(const vvi& g, vvi& d){
-    for(size_t k=0; k<g.size(); k++){
-        for(size_t u=0; u<g.size(); u++){
-            for(size_t v=0; v<g.size(); v++){
-                d[u][v] = min(d[u][v], d[u][k]+d[k][v]);
-            }
-        }
-    }
-}
-
-void dfs(vvi&g, int s, int limit, vector<bool>& visible, bool& hit, int o){
-    if(limit == 0){
-        if(s == o){
-            hit = true;
-        }
-        return;
-    }
-    for(int u:g[s]) {
-        if (!visible[u]) {
-            // cout << "limit: " << limit << " " << s << "-->" << u << endl;
-            visible[u] = true;
-            dfs(g, u, limit-1, visible, hit, o);
-            visible[u] = false;
-        }
-    }
-}
-
-bool dfs2(vvi &g, vector<int> &col, int node, int c) {
+/* exists a cycle of odd length */
+bool dfs(vvi &g, vector<int> &col, int node, int c) {
   if (col[node] != -1)
     return col[node] == c;
 
   col[node] = c;
   for (auto &to : g[node]) {
-    if (!dfs2(g, col, to, c ^ 1))
+    if (!dfs(g, col, to, c ^ 1))
       return false;
   }
   return true;
-}
-
-bool limited_cycle(vvi& g, int s, int limit){
-    vector<bool> visible;
-    bool hit = false;
-    visible.assign(g.size(), false);   
-
-    // dfs(g, s, limit, visible, hit, s);
-
-    // cout << "limited cycle: " << hit << endl;
-    return hit;
 }
 
 /* ================================================= */
@@ -275,7 +158,6 @@ int main(){
         for(int i=0; i<w; i++){
             point well;
             scanf("%lf %lf", &well.x, &well.y);
-            // cout << "well: " << well.x << "," << well.y << endl;
             /* accumulate wells */
             wells.push_back(well);
         }
@@ -290,12 +172,6 @@ int main(){
         }
         
         /* Create intersection graph */
-        // vvi d(p, vi(p, INT16_MAX));
-        // vector<vector<bool>> intersection_map(p, vector<bool>(p, false));
-        // find_intersections(pipes, intersection_map);
-        
-        /* ====== */ 
-
         g.assign(p, vi());
         for(size_t i=0; i<pipes.size(); i++){
             for(size_t j=i+1; j<pipes.size(); j++){
@@ -303,37 +179,17 @@ int main(){
                 if(pipes[i].a == pipes[j].a){
                     continue;
                 }
-
-                // if(SegmentsIntersect(PT(pipes[i].a.x, pipes[i].a.y), PT(pipes[i].b.x, pipes[i].b.y),
-                //                     PT(pipes[j].a.x, pipes[j].a.y), PT(pipes[j].b.x, pipes[j].b.y))){
-
                 if(segmentIntersect(pipes[i], pipes[j], p)){
-                    // cout << "intersection found: " << endl;
-                    // cout << pipes[i].a.x << "," << pipes[i].a.y;
-                    // cout << "---" << pipes[i].b.x << "," << pipes[i].b.y;
-                    // cout << " X " << pipes[j].a.x << "," << pipes[j].a.y;
-                    // cout << "---" << pipes[j].b.x << "," << pipes[j].b.y << endl;
                     g[i].push_back(j);
                     g[j].push_back(i);
-                    // cout << i << "->" << j << " added" << endl;
                 }
             }
         }
 
-        // int k=0;
-        // for(auto u:g){
-        //     cout << k << ": ";
-        //     for(auto v:u){
-        //         cout << " " << v;
-        //     }
-        //     cout << endl;
-        //     k++;
-        // }
-
         vector<int> col(p, -1);
         for (int i = 0; i < p; ++i) {
             if (col[i] == -1){
-                if(!dfs2(g, col, i, 0)){
+                if(!dfs(g, col, i, 0)){
                     impossible = true;
                     break;
                 }
@@ -346,10 +202,7 @@ int main(){
             cout << "possible" << endl; 
         }
 
-
     }
-    
-
 
     return 0;
 }
