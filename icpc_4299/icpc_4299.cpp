677b2d4f30fa9c634aed8fe23ee042056303d506
DESKTOP-CO549O6\slous <simon.lousky@campus.technion.ac.il>
2019-06-24 14:35:24 +0300

    4299 working

--- icpc_4299/icpc_4299.cpp
+++ icpc_4299/icpc_4299.cpp
@@ -16,9 +16,11 @@ typedef vector<vi> vvi;
 typedef long long ll;
 typedef vector<ll> vl;
 typedef vector<vl> vvl;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
 
 /* Floyd-Warshal */
-void floyd(const vvl& g, vvl& d){
+void floyd(const vvi& g, vvi& d){
     for(size_t k=0; k<g.size(); k++){
         for(size_t u=0; u<g.size(); u++){
             for(size_t v=0; v<g.size(); v++){
@@ -33,15 +35,15 @@ int main(){
     scanf("%d", &T);
     for (int t = 0; t < T; t++){
         int N, R;
-        vvl d, g;
+        vvi d, g;
         
         scanf("%d %d", &N, &R);
         for(int i=0; i<N; i++){
             string connections;
             cin >> connections;
 
-            d.push_back(vl());
-            g.push_back(vl());
+            d.push_back(vi());
+            g.push_back(vi());
             for(int j=0; j<N; j++){
                 int w = connections[j] == 'N' ? MAX_DISTANCE : 1;
                 
@@ -57,21 +59,23 @@ int main(){
         /* obtain min stations between every point */
         floyd(d, d);
 
-        vvl ref(105, vl(10005, 0));
+/* test with doubles */
+        vvd ref(105, vd(10005, 0));
         for (int i=0; i<105 && i<d.size(); i++){
             for(int j=0; j<10005 && j<(i+1)*R; j++){
                 if(i==0){
-                    ref[i][j] = 1;
+                    ref[i][j] = 1.0/R;
                     continue;
                 }
                 for(int k=1; k<=R; k++){
                     if(j-k < 0){
                         break;
                     }
-                    ref[i][j] += ref[i-1][j-k];
+                    ref[i][j] += ref[i-1][j-k] / R;
                 }
             }
         }
+
         int C;
         scanf("%d", &C);
         printf("Case %d\n", t+1);
@@ -80,27 +84,17 @@ int main(){
             double res = 0.0;
             scanf("%d %d %d", &a, &b, &m);
             dist = d[a-1][b-1];
-
-            for(int k=0; k<m; k++){
-                res += ref[dist-1][k] / pow(R,dist);
-            }
             
-            if(dist > 99)
-            {
-                res = 0.0;
-            }else if(a == b || (m >= pow(R, dist) && dist <= m)){
-                // if(t==83){
-                //     cout << "dist: " << dist << ", R: " << R << endl;
-                //     cout << "a: " << a << ", b: " << b << ", m: " << m << endl;
-                // }
-                res = 1.0;
+            if (dist < 100 && m <= 10005){
+                for(int k=0; k<m; k++){
+                    res += ref[dist-1][k];
+                }
             }
+
             cout << setprecision(6) << fixed;
             cout << res << endl;
         }
-        // if(t != T-1){
-            printf("\n");
-        // }
+        printf("\n");
     }
 
     return 0;
